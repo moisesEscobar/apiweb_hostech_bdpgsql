d347b1b4d0a045ba8a08477cc69822c241251f15
Moises Escobar Martinez <tecnologia7@hostech.com.mx>
2023-12-28 10:28:37 -0600

    add: add news procedures and new tables

--- functions.sql
+++ functions.sql
@@ -1,11 +1,52 @@
-DROP Function IF EXISTS create_log;
-CREATE OR REPLACE FUNCTION public.create_log(p_user_id integer, p_action character varying, p_catalog character varying, p_detail_last jsonb, p_detail_new jsonb)
- RETURNS void
- LANGUAGE plpgsql
+
+DROP Function IF EXISTS insert_into_payment_order_txn;
+CREATE OR REPLACE FUNCTION public.insert_into_payment_order_txn(p_status character varying, p_amount double precision, p_user_id integer, p_payment_type_id integer, p_payment_order_id integer, p_supplier_customer_id integer)
+RETURNS json
+LANGUAGE plpgsql
 AS $function$
-        BEGIN
-            INSERT INTO logs (user_id, action, catalog, detail_last, detail_new, created_at,updated_at)
-            VALUES (p_user_id, p_action, p_catalog, p_detail_last, p_detail_new, NOW(),NOW());
-        END;
-        $function$
+DECLARE v_new_record payment_order_txn;
+DECLARE v_new_record_id INT;
+BEGIN
+    -- Validar payment_type_id
+    IF NOT EXISTS (SELECT 1 FROM payment_types WHERE id = p_payment_type_id) THEN
+        RAISE EXCEPTION 'payment_type_id % no existe', p_payment_type_id;
+    END IF;
+    -- Validar payment_order_id
+    IF NOT EXISTS (SELECT 1 FROM payment_orders WHERE id = p_payment_order_id) THEN
+        RAISE EXCEPTION 'payment_order_id % no existe', p_payment_order_id;
+    END IF;
+    -- Validar supplier_customer_id
+    IF NOT EXISTS (SELECT 1 FROM suppliers_customers WHERE id = p_supplier_customer_id) THEN
+        RAISE EXCEPTION 'supplier_customer_id % no existe', p_supplier_customer_id;
+    END IF;
+	
+	-- Verificar duplicados
+    IF EXISTS (SELECT 1 FROM payment_order_txn where
+        payment_type_id = p_payment_type_id 
+            AND payment_order_id = p_payment_order_id 
+            AND supplier_customer_id = p_supplier_customer_id) THEN
+        RAISE EXCEPTION 'Un registro con los mismos payment_type_id, payment_order_id y supplier_customer_id ya existe';
+    END IF;
+
+    -- Realizar la inserción en payment_order_txn
+    INSERT INTO payment_order_txn (user_id, payment_type_id, payment_order_id, supplier_customer_id, status, amount, created_at, updated_at)
+    VALUES (p_user_id, p_payment_type_id, p_payment_order_id, p_supplier_customer_id, p_status, p_amount, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+    RETURNING id INTO v_new_record_id;
+
+    -- Devolver el registro creado en formato JSON
+    RETURN (SELECT json_build_object(
+        'id', id, 
+        'status', status,
+        'user_id', user_id,
+        'payment_type_id', payment_type_id,
+        'payment_order_id', payment_order_id,
+        'supplier_customer_id', supplier_customer_id,
+        'amount', amount,
+        'created_at', created_at,
+        'updated_at', updated_at
+    ) FROM payment_order_txn WHERE id = v_new_record_id);
+EXCEPTION
+    WHEN others THEN RAISE;
+END;
+$function$
 ;
--- views.sql
+++ views.sql
@@ -1,77 +1,205 @@
 CREATE
-OR REPLACE VIEW public.view_users AS
-SELECT
-    users.id,
-    users.name,
-    users.last_name,
-    users.second_surname,
-    users.email,
-    users.password,
-    users.created_at,
-    users.updated_at,
-    users.deleted_at
-FROM users
-WHERE (users.deleted_at IS NULL);
-
-CREATE
-OR REPLACE VIEW public.view_brands AS
-SELECT
-    brands.id,
-    brands.name,
-    brands.created_at,
-    brands.updated_at,
-    brands.deleted_at
-FROM brands
-WHERE (brands.deleted_at IS NULL);
-
-CREATE
 OR REPLACE VIEW public.view_suppliers AS
 SELECT
-    suppliers.id,
-    suppliers.name,
-    suppliers.created_at,
-    suppliers.updated_at,
-    suppliers.deleted_at
-FROM suppliers
-WHERE (suppliers.deleted_at IS NULL);
+    id,
+    name,
+    phone_number,
+    description,
+    type_user,
+    created_at,
+    updated_at,
+    deleted_at
+FROM suppliers_customers
+WHERE (deleted_at IS NULL);
+
+CREATE
+OR REPLACE VIEW public.view_payment_orders AS
+SELECT
+    id,
+    status,
+    payment_date,
+    created_at,
+    updated_at,
+    deleted_at
+FROM payment_orders
+WHERE (deleted_at IS NULL);
 
 CREATE
 OR REPLACE VIEW public.view_payment_types AS
 SELECT
-    payment_types.id,
-    payment_types.name,
-    payment_types.created_at,
-    payment_types.updated_at,
-    payment_types.deleted_at
+    id,
+    name,
+    created_at,
+    updated_at,
+    deleted_at
 FROM payment_types
-WHERE (
-        payment_types.deleted_at IS NULL
-    );
+WHERE (deleted_at IS NULL);
+
+CREATE
+OR REPLACE VIEW public.view_purchase_orders AS
+SELECT
+    shopping_id,
+    payment_order_id,
+    created_at,
+    updated_at,
+    deleted_at
+FROM purchase_orders
+WHERE (deleted_at IS NULL);
+
+CREATE
+OR REPLACE VIEW public.view_shoppings AS
+SELECT
+    id,
+    inventory_id,
+    unit_price,
+    created_at,
+    updated_at,
+    deleted_at
+FROM shoppings
+WHERE (deleted_at IS NULL);
+
+CREATE
+OR REPLACE VIEW public.view_brands AS
+SELECT
+    id,
+    name,
+    created_at,
+    updated_at,
+    deleted_at
+FROM brands
+WHERE deleted_at IS NULL;
 
 CREATE
 OR REPLACE VIEW public.view_products AS
 SELECT
-    prds.id,
-    prds.name,
-    prds.sku,
-    prds.price,
-    prds.reorder_point,
-    prds.brand_id, (
+    id,
+    name,
+    sku,
+    price,
+    reorder_point,
+    brand_id, (
         SELECT brands.name
         FROM brands
         WHERE (brands.id = prds.brand_id)
     ) AS brand_name,
-    prds.supplier_id, (
-        SELECT suppliers.name
-        FROM suppliers
+    supplier_customer_id, (
+        SELECT
+            suppliers_customers.name
+        FROM
+            suppliers_customers
         WHERE (
-                suppliers.id = prds.supplier_id
+                suppliers_customers.id = prds.supplier_customer_id
             )
     ) AS supplier_name,
-    prds.created_at,
-    prds.updated_at
+    created_at,
+    updated_at
 FROM products prds
-WHERE (prds.deleted_at IS NULL);
+WHERE (deleted_at IS NULL);
+
+CREATE
+OR REPLACE VIEW public.view_payment_order_txns AS
+SELECT
+    id,
+    status,
+    amount,
+    user_id,
+    payment_type_id,
+    payment_order_id,
+    supplier_customer_id,
+    created_at,
+    updated_at,
+    deleted_at
+FROM payment_order_txn
+WHERE deleted_at IS NULL;
+
+CREATE
+OR REPLACE VIEW public.view_users AS
+SELECT
+    id,
+    name,
+    last_name,
+    second_surname,
+    phone_number,
+    email,
+    password,
+    created_at,
+    updated_at,
+    deleted_at
+FROM users
+WHERE (deleted_at IS NULL);
+
+CREATE
+OR REPLACE VIEW public.view_brands_with_products AS
+SELECT
+    id,
+    name,
+    created_at,
+    updated_at,
+    deleted_at
+FROM view_brands vb
+WHERE (
+        EXISTS (
+            SELECT 1
+            FROM products
+            WHERE
+                vb.id = products.brand_id
+        )
+    );
+
+CREATE
+OR REPLACE VIEW public.view_inventories AS
+SELECT
+    iv.id,
+    vp.supplier_customer_id, (
+        SELECT
+            suppliers_customers.name
+        FROM
+            suppliers_customers
+        WHERE
+            suppliers_customers.id = vp.supplier_customer_id
+    ) AS supplier_name,
+    iv.product_id,
+    vp.name,
+    vp.sku,
+    vp.brand_name,
+    iv.quantity,
+    vp.price,
+    vp.reorder_point,
+    iv.created_at,
+    iv.updated_at
+FROM inventories iv
+    JOIN view_products vp ON iv.product_id = vp.id
+WHERE iv.deleted_at IS NULL;
+
+CREATE
+OR REPLACE VIEW public.view_inventory_resume AS
+SELECT
+    ip.product_id,
+    ip.total_quantity,
+    COALESCE(ips.quantity_sold, 0 :: bigint) AS quantity_sold,
+    COALESCE(
+        ips.total_amount,
+        0 :: double precision
+    ) AS total_amount
+FROM (
+        SELECT
+            inventories.product_id,
+            sum(inventories.quantity) AS total_quantity
+        FROM inventories
+        WHERE
+            inventories.deleted_at IS NULL
+        GROUP BY
+            inventories.product_id
+    ) ip
+    LEFT JOIN (
+        SELECT
+            product_sales.product_id,
+            sum(product_sales.quantity) AS quantity_sold,
+            sum(product_sales.total_amount) AS total_amount
+        FROM product_sales
+        GROUP BY
+            product_sales.product_id
+    ) ips ON ip.product_id = ips.product_id;
 
 CREATE
 OR REPLACE VIEW public.view_logs AS
@@ -87,84 +215,81 @@ SELECT
     u.email AS user_email,
     lv.created_at,
     lv.updated_at
-FROM (
-        logs lv
-        JOIN users u ON ( (lv.user_id = u.id))
-    )
-WHERE (lv.deleted_at IS NULL)
+FROM logs lv
+    JOIN users u ON lv.user_id = u.id
+WHERE lv.deleted_at IS NULL
 ORDER BY lv.id DESC;
 
 CREATE
-OR REPLACE VIEW public.view_brands_with_products AS
+OR REPLACE VIEW public.view_shoppings_inventories AS
 SELECT
-    vb.id,
-    vb.name,
-    vb.created_at,
-    vb.updated_at,
-    vb.deleted_at
-FROM view_brands vb
-WHERE (
-        EXISTS (
-            SELECT 1
-            FROM products
-            WHERE (vb.id = products.brand_id)
-        )
-    );
-
-CREATE
-OR REPLACE VIEW public.view_inventory_resume AS
-SELECT
-    ip.product_id,
-    ip.total_quantity,
-    COALESCE(
-        ips.quantity_sold, (0) :: bigint
-    ) AS quantity_sold,
-    COALESCE(ips.total_amount, (0) :: bigint) AS total_amount
-FROM ( (
-            SELECT
-                inventories.product_id,
-                sum(inventories.quantity) AS total_quantity
-            FROM inventories
-            WHERE (inventories.deleted_at IS NULL)
-            GROUP BY
-                inventories.product_id
-        ) ip
-        LEFT JOIN (
-            SELECT
-                product_sales.product_id,
-                sum(product_sales.quantity) AS quantity_sold,
-                sum(product_sales.total_amount) AS total_amount
-            FROM
-                product_sales
-            GROUP BY
-                product_sales.product_id
-        ) ips ON ( (ip.product_id = ips.product_id)
-        )
-    );
-
-CREATE
-OR REPLACE VIEW public.view_inventories AS
-SELECT
-    iv.id,
-    vp.supplier_id, (
-        SELECT suppliers.name
-        FROM suppliers
-        WHERE (suppliers.id = vp.supplier_id)
-    ) AS supplier_name,
-    iv.product_id,
-    vp.name,
-    vp.sku,
-    vp.brand_name,
-    iv.quantity,
-    vp.price,
-    vp.reorder_point,
-    iv.created_at,
-    iv.updated_at
+    vsh.id AS shopping_id,
+    vsh.inventory_id,
+    vsh.unit_price,
+    vi.supplier_customer_id,
+    vi.supplier_name,
+    vi.product_id,
+    vi.name AS product_name,
+    vi.sku AS product_sku,
+    vi.brand_name,
+    vi.quantity AS product_quantity, ( (vi.quantity) :: double precision * vsh.unit_price
+    ) AS total_amount
 FROM (
-        inventories iv
-        JOIN view_products vp ON ( (iv.product_id = vp.id))
-    )
-WHERE (iv.deleted_at IS NULL);
+        view_shoppings vsh
+        JOIN view_inventories vi ON ( (vsh.inventory_id = vi.id))
+    );
+
+CREATE
+OR REPLACE VIEW public.view_payment_orders_purchase AS
+SELECT
+    vpu.payment_order_id,
+    vpo.status,
+    vpo.payment_date,
+    vsi.shopping_id,
+    vsi.inventory_id,
+    vsi.unit_price,
+    vsi.supplier_customer_id,
+    vsi.supplier_name,
+    vsi.product_id,
+    vsi.product_name,
+    vsi.product_sku,
+    vsi.brand_name,
+    vsi.product_quantity,
+    vsi.total_amount
+FROM ( (
+            view_purchase_orders vpu
+            JOIN view_payment_orders vpo ON ( (vpu.payment_order_id = vpo.id)
+            )
+        )
+        JOIN view_shoppings_inventories vsi ON ( (
+                vpu.shopping_id = vsi.shopping_id
+            )
+        )
+    );
+
+CREATE
+OR REPLACE VIEW public.view_products_with_inventory AS
+SELECT
+    vp.id AS product_id,
+    vp.supplier_customer_id,
+    vp.supplier_name,
+    vp.name AS product_name,
+    vp.sku AS product_sku,
+    vp.brand_id,
+    vp.brand_name,
+    vp.price AS product_price,
+    vp.reorder_point AS product_reorder_point,
+    vir.total_quantity,
+    vir.quantity_sold, (
+        vir.total_quantity - vir.quantity_sold
+    ) AS quantity_available,
+    COALESCE(
+        vir.total_amount, (0) :: double precision
+    ) AS total_amount
+FROM (
+        view_products vp
+        JOIN view_inventory_resume vir ON ( (vp.id = vir.product_id))
+    );
 
 CREATE
 OR REPLACE VIEW public.view_product_sales AS
@@ -183,25 +308,3 @@ FROM (
         JOIN view_products vp ON ( (ps.product_id = vp.id))
     )
 WHERE (ps.deleted_at IS NULL);
-
-CREATE
-OR REPLACE VIEW public.view_products_with_inventory AS
-SELECT
-    vp.id AS product_id,
-    vp.supplier_id,
-    vp.supplier_name,
-    vp.name AS product_name,
-    vp.sku AS product_sku,
-    vp.brand_id,
-    vp.brand_name,
-    vp.price AS product_price,
-    vp.reorder_point AS product_reorder_point,
-    vir.total_quantity,
-    vir.quantity_sold, (
-        vir.total_quantity - vir.quantity_sold
-    ) AS quantity_available,
-    COALESCE(vir.total_amount, (0) :: bigint) AS total_amount
-FROM (
-        view_products vp
-        JOIN view_inventory_resume vir ON ( (vp.id = vir.product_id))
-    );
